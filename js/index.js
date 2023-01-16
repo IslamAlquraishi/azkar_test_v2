9cbc5040e1ddc1ce04537682700d36d34cf29259
IslamAl-Quraishi <eslamalsayed8133@gmail.com>
2023-01-16 19:25:11 +0200

    application azker not finish yet v.2

--- js/index.js
+++ js/index.js
@@ -43,3 +43,60 @@ function get() {
   }
 }
 window.addEventListener("resize", get);
+
+let info_team_inner = `
+      <div id="info_team" class="info_team">
+      <div>
+        Developer:
+        <a href="https://www.facebook.com/Isl7mAlQuraishi"
+          >alquraishi <i class="fa-brands fa-facebook"></i
+        ></a>
+        <img src="images/photo1.jpg" alt="Islam_AlQuraishi.img" />
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+      </div>
+
+      <div>
+        Desginer:
+        <a href="">sara khaled <i class="fa-brands fa-facebook"></i></a>
+        <img src />
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+      </div>
+    </div>
+  `;
+
+let close_info = `
+  <span id="xmark" class="xmark"><i class="fa-solid fa-xmark"></i> Close</span>
+  `;
+
+let toggle_info = document.getElementById("toggle_info");
+let info_team = document.getElementById("info_team");
+
+toggle_info.addEventListener("click", () => {
+  inner(info_team_inner, close_info);
+  setInterval(() => {
+    document.querySelector("#info_team").classList.add("active1");
+  }, 250);
+});
+
+function inner(info_team_inner, close_info) {
+  let ele_inner = document.createElement("div");
+  ele_inner.setAttribute("id", "inner");
+  ele_inner.innerHTML = info_team_inner;
+
+  setInterval(() => {
+    ele_inner.innerHTML += close_info;
+  }, 2500);
+
+  document.querySelector("footer").before(ele_inner);
+}
+
+document.addEventListener("click", (e) => {
+  if (e.target == xmark) {
+    setInterval(() => {
+      document.querySelector("#info_team").classList.add("active2");
+    }, 250);
+    setInterval(() => {
+      document.querySelector("#inner").remove();
+    }, 1500);
+  }
+});
